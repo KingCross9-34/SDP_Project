a282040e989bc62ef8c80aca3978715fb1ab7980
vege-yummy <zjm@zjmdeMacBook-Pro.local>
2021-11-29 14:20:14 +0800

    风险预测接口

--- HelloWorld/personal/urls.py
+++ HelloWorld/personal/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('getUserById/<int:id>',views.getUser)
+    path('getUserById/<int:id>',views.getUser),
+    path('svm_predict',views.predict_svm),
+    path('mlp_predict',views.predict_mlp),
+    path('logistic_predict',views.predict_logictic)
+
 ]
--- HelloWorld/personal/views.py
+++ HelloWorld/personal/views.py
@@ -1,15 +1,16 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
 import pandas as pd
 from django.http import HttpResponse
 import json
+import joblib
 # Create your views here.
 
 
 #http://127.0.0.1:8000/personal/getUserById/id
 #按照id搜索单个用户
 def getUser(request,id):
-    print(request)
-    data=pd.read_csv('static/data.csv',nrows=8000)
+    data=pd.read_csv('static/data.csv')
     tmp=data.loc[id]
     result=tmp.to_json(orient="index")
     parsed = json.loads(result)
@@ -17,3 +18,41 @@ def getUser(request,id):
     return HttpResponse(json.dumps(parsed)) #返回该用户的所有信息
 
 
+def predict_svm(request):
+    id=int(request.POST['id'])
+    data=pd.read_csv('static/final.csv')
+    svm=joblib.load('static/model/svm.pkl')
+    x=data.loc[id]
+    result=svm.predict([x.to_numpy()])[0]
+    object={
+        'risk':int(result)
+    }
+    return JsonResponse(object)
+
+
+def predict_mlp(request):
+    id=int(request.POST['id'])
+    data=pd.read_csv('static/final.csv')
+    mlp=joblib.load('static/model/mlp.pkl')
+    x=data.loc[id]
+    result=mlp.predict([x.to_numpy()])[0]
+    object={
+        'risk':int(result)
+    }
+    return JsonResponse(object)
+    
+    
+
+
+
+def predict_logictic(request):
+    id=int(request.POST['id'])
+    data=pd.read_csv('static/final.csv')
+    logictic=joblib.load('static/model/logisticRegression.pkl')
+    x=data.loc[id]
+    result=logictic.predict([x.to_numpy()])[0]
+    object={
+        'risk':int(result)
+    }
+    return JsonResponse(object)
+    
